a0af68a8437e130aaef0bfe983a7546e079b0d1e
bk20x <bkaenel4@gmail.com>
2025-10-11 01:47:18 -0500

    add test

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,9 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
 
 import uleb128
-test "can add":
-  check add(5, 5) == 10
+test "can decode":
+  let
+    data: uint = 29912
+    encoded = uleb128Encode data
+    decoded = uleb128Decode encoded
+  check (decoded == data)
